2b98dce99bb8115eba9c26148e680031a08a8532
Pradip Gareja <pradipgareja@Pradips-MacBook-Air.local>
2021-08-10 16:58:29 +0530

    bgp

---
+++ bgp_napalm/bgp.py
@@ -0,0 +1,7 @@
+from napalm import get_network_driver
+driver=get_network_driver('ios')
+iosvl2=driver("ip",'username','pass')
+iosvl2.open()
+ios_ouput=iosvl2.get_facts()
+print(ios_ouput)
+ios_ouput2=iosvl2.get_bgp_neighbours()
